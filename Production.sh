b79187b177af14366e4a3746368bf3491f13e431
tom jones <rp_tomj@hotmail.com>
2021-03-07 10:44:52 -0800

    production scripts

--- Production.sh
+++ Production.sh
@@ -1,1 +1,3 @@
-﻿docker-compose -f docker-compose.yml -f production.yml build
+﻿git checkout stash@{0} -- production.yml
+git checkout stash@{0} -- Dockerfile
+docker-compose -f docker-compose.yml -f production.yml build
